11f6dfa6f00920c24e3690ffa4edfc9ec3782824
cest713 <t.x.w@163.com>
2012-01-09 20:59:43 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -420,7 +420,7 @@ withjQuery(function($){
 		setTimeout(submitForm, 500);
 	}
 	function reloadSeat(){
-		$("select[name$='_seat']").html('<option value="M" selected="">一等座</option><option value="O" selected="">二等座</option><option value="1">硬座</option><option value="3">硬卧</option><option value="4">软卧</option>');
+		$("select[name$='_seat']").html('<option value="M">一等座</option><option value="O">二等座</option><option value="1">硬座</option><option value="3"  selected>硬卧</option><option value="4">软卧</option>');
 	}
 	//初始化
     if($("#refreshButton").size()<1){
