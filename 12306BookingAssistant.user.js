8076ddbe27c01b1adec61984a3441699c439b247
cest713 <t.x.w@163.com>
2012-01-09 23:59:57 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -420,7 +420,7 @@ withjQuery(function($){
 	function reSubmitForm(){
 		count ++;
 		$('#refreshButton').html("("+count+")次自动提交中...");
-		alert($('#passenger_1_seat').val);
+		alert($('#passenger_1_seat').attr('value'));
 		setTimeout(submitForm, 500);
 	}
 	function reloadSeat(){
