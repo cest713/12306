3dbd8f202904b84c443bd1cd2dbd998f6748dfd4
cest713 <t.x.w@163.com>
2012-01-11 19:05:18 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -307,7 +307,7 @@ withjQuery(function($){
 		//Check had login, redirect to query url
 		if( parent && parent.$ ) {
 			var str = parent.$("#username_ a").attr("href");
-			if( str && str.indexOf("sysuser/user_info") != -1&&str.indexof( ){	
+			if( str && str.indexOf("sysuser/user_info") != -1&&str.indexof( )){	
 				//如果是点击的是我的12306,要不跳转				
 			        if(window.location.href.indexOf("initForMy12306") != -1 )
 			        return;
