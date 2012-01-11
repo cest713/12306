b214f9ffd73802c327325a1fce44f4eb5afc600c
cest713 <t.x.w@163.com>
2012-01-11 18:38:37 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -303,7 +303,7 @@ withjQuery(function($){
 	route("login", function() {
 		//login
 		if(window.location.href.indexOf("initForMy12306") != -1 ) {
-		 if(parent.notlogin==undefined)return;
+		 if(parent.$("#loginForm")==undefined)return;
 		}
 		var url = "https://dynamic.12306.cn/otsweb/loginAction.do?method=login";
 		var queryurl = "https://dynamic.12306.cn/otsweb/order/querySingleAction.do?method=init";
