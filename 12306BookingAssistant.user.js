dd17ae862fe68ab8389ac7b1a3e2f855a7aaab73
cest713 <t.x.w@163.com>
2012-09-19 19:15:34 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -131,7 +131,42 @@ withjQuery(function($){
  		
  			var logrnd;
    			var logerr;
-   		
+   		        $.ajax({
+     				type: "POST",
+     				async:false, 
+     				url: "/otsweb/loginAction.do?method=loginAysnSuggest",
+     				data: {
+     				//	"loginUser.user_name": 
+
+                                 //  $("#UserName").val()
+     				//  , "user.password":  $("#password").val()
+     				//  , "randCode": $("#randCode").val()
+     				},
+     				timeout: 30000,
+     				//cache: false,
+     				//async: false,
+     				success: function(msg){
+     					//alert(msg);
+     					var suggest = eval("(" + msg + ")");
+     					//alert(suggest);
+     					//alert(suggest.loginRand);
+     					logrnd = suggest.loginRand;
+     					$("#loginRand").val = logrnd;
+     					alert($("#loginRand").val);
+     					logerr = suggest.randError;
+     			 	        //alert(logerr);
+     			 	        return logrnd;
+     				 					
+     				},
+     				error: function(msg){
+     					//reLogin();
+     				},
+     				beforeSend: function(XHR){
+     					//alert("Data Saved: " + XHR);
+     				
+     				}
+     			});
+     			alert("1"+logrnd);
     			$.ajax({
  				type: "POST",
  				url: submitUrl,
