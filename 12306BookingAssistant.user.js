c877826008a55911f6b793b11fa91c23eab6b94a
cest713 <t.x.w@163.com>
2012-09-23 22:26:32 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -31,9 +31,9 @@
 
 // ==UserScript==  
 // @name         12306 Booking Assistant
-// @version		 1.3.8
-// @author       zzdhidden@gmail.com
-// @namespace    https://github.com/zzdhidden
+// @version		    1.4
+// @author       cest713
+// @namespace    https://github.com/cest713
 // @description  12306 订票助手之(自动登录，自动查票，自动订单)
 // @include      *://dynamic.12306.cn/otsweb/*
 // @require	https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
