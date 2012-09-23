0cdd53c7e610efa260cf8960f7332cca2c532a07
cest713 <t.x.w@163.com>
2012-09-23 12:42:12 +0800

    Update 12306BookingAssistant.user.js

--- 12306BookingAssistant.user.js
+++ 12306BookingAssistant.user.js
@@ -464,7 +464,7 @@
   						//}
                                                     
   						if(msg != null){
-   					   if( msg.waitTime<=0 ) {
+   					   if( msg.waitTime<=0 &&msg.orderId<>null) {
   							//Success!
   							   alert("车票预订成功，恭喜!");
   							   notify("车票预订成功，恭喜!",500);
